9d27cbe0ecc17a23a7047e87feab01d958cef6e7
Akshay <akshay.haryani@gmail.com>
2020-05-20 17:54:59 -0700

    Adding Report Interface

---
+++ src/main/java/com/designPatterns/safedec/ReportInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.designPatterns.safedec;
+
+/**
+ *
+ * @author akshayharyani
+ */
+public interface ReportInterface {
+    
+    void generateReport();
+    
+}
