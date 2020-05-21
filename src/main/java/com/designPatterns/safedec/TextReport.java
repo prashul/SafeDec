8be1d2a800eb686ebda6ce4b8b7e3ca9cb61bbfd
Akshay <akshay.haryani@gmail.com>
2020-05-20 17:50:36 -0700

    Adding Report Classes

---
+++ src/main/java/com/designPatterns/safedec/TextReport.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.designPatterns.safedec;
+
+/**
+ *
+ * @author akshayharyani
+ */
+public class TextReport implements ReportInterface {
+    
+    public void generateReport(){
+        
+    }
+    
+}
